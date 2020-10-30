f4451db7355dc05c100fe25c1bbd93692ba5e3be
Trafle <igorokkarpiuk@gmail.com>
2020-10-30 16:19:47 +0200

    PostfixToPrefix rename and check

--- implementation.go
+++ implementation.go
@@ -3,7 +3,7 @@ package lab2
 import "fmt"
 
 // TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
+// PostfixToPrefix converts
+func PostfixToPrefix(input string) (string, error) {
 	return "TODO", fmt.Errorf("TODO")
 }
