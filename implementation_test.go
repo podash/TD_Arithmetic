e520badf56e99ef7d5cc1af69fd4e65fb106006e
MaryLynJuana <mary_juana@ukr.net>
2020-12-26 17:40:27 +0200

    refactor(tests): Change examples to []struct

--- implementation_test.go
+++ implementation_test.go
@@ -10,23 +10,25 @@ func TestImplementation (t *testing.T) { TestingT(t) }
 type TestSuite struct{}
 
 func (s *TestSuite) TestPostfixToPrefix (c *C) {
-	examples := map[string]string {
-		"2 45 + 6 + 24 +": "+ + + 2 45 6 24",
-		"60000321 764 +": "+ 60000321 764",
-		"26 89 13 +": "too many operands",
-		"98 4 * 234 12 * +": "+ * 98 4 * 234 12",
-		"hello expression": "too many operators",
-		"9.007 765.9999994 + 56 ^ /": "too many operators",
-		"": "invalid input expression",
-		"4 2 - 3 * 5+": "+ * - 4 2 3 5",
+	examples := []struct {
+		postfix, expected string
+	} {
+		{ "2 45 + 6 + 24 +", "+ + + 2 45 6 24" },
+		{ "60000321 764 +", "+ 60000321 764" },
+		{ "26 89 13 +", "too many operands" },
+		{ "98 4 * 234 12 * +", "+ * 98 4 * 234 12" },
+		{ "hello expression", "too many operators" },
+		{ "9.007 765.9999994 + 56 ^ /", "too many operators" },
+		{ "", "invalid input expression" },
+		{ "4 2 - 3 * 5+", "+ * - 4 2 3 5" },
 	}
 
-	for postfix, expected := range examples {
-		res, err := PostfixToPrefix(postfix)
+	for _, ex := range examples {
+		res, err := PostfixToPrefix(ex.postfix)
 		if err != nil {
-			c.Assert(err, ErrorMatches, expected)
+			c.Assert(err, ErrorMatches, ex.expected)
 		} else {
-			c.Assert(res, Equals, expected)
+			c.Assert(res, Equals, ex.expected)
 		}
 	}
 }
